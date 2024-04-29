239a9e93660cdf475cc4a92bb1e0741c7f2b8eb1
yav <796176@protonmail.com>
2024-04-29 18:03:12 +0300

    Add members into BackupEngineInterface

--- src/main/java/jbackup/backend/BackupEngineInterface.java
+++ src/main/java/jbackup/backend/BackupEngineInterface.java
@@ -1,4 +1,9 @@
 package jbackup.backend;
 
+import java.io.*;
+
 public interface BackupEngineInterface {
+	void createBackup(File source, File dst, File[] exceptions);
+	void createBackup(File source, File dst);
+	BackupStatus getStatus();
 }
